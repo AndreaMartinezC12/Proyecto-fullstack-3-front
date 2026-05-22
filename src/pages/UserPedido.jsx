19c705394bd6cd0471a7d0521f0ffeb834caa74b
AndreaMartinezC12 <andre.martcer@gmail.com>
2026-05-22 14:00:30 -0600

    Agregando formato a la pantalla del pedido del usuario

--- src/pages/UserPedido.jsx
+++ src/pages/UserPedido.jsx
@@ -7,6 +7,7 @@ import api from "../api/axios";
 export default function UserPedido(){
     const[pedido, setPedido] = useState(null)
     const navigate = useNavigate()
+    const user= JSON.parse(localStorage.getItem('user'))
 
     const logout = () => {
         localStorage.removeItem('token')
@@ -34,61 +35,63 @@ export default function UserPedido(){
     // }
 
     return(
-        <div>
-            {!pedido && (
-                <div>
-                    <h2>Aun no has hecho un pedido</h2>
-                    <button onClick={() => navigate("/new-order")}>
-                        Nuevo Pedido
-                    </button>
-                </div>
-            )}
-
-            {pedido &&(
-                <div className="result-card">
-                    <h2>Resultado de la busqueda</h2>
-                    <div className="section">
-                        <h3>Informacion del cliente</h3>
-                        <p>Nombre: {" "} {pedido.cliente.nombre}</p>
-                        <p>Email: {" "} {pedido.cliente.email}</p>
-                        <p>Telefono: {" "} {pedido.cliente.telefono}</p>
+        <div className="home-container">    
+                <h2 className="welcome-title">Bienvenido: {user?.nombre} </h2>
+                <p className="user-email">Email: {user?.email}</p>
+                {!pedido && (
+                    <div className="home-card">
+                        <h2 className="empty-message">Aun no has hecho un pedido</h2>
+                        <button className="create-button" onClick={() => navigate("/new-order")}>
+                            Nuevo Pedido
+                        </button>
                     </div>
-                    <div className="section">
-                        <h3>Informacion del destinatario</h3>
-                        <p>Nombre:{" "}{pedido.destinatario.nombre}</p>
-                        <p>Direccion:
-                            {" "}{pedido.destinatario.calle}
-                            {" "}{pedido.destinatario.numero}
-                            {" "}{pedido.destinatario.colonia}
-                        </p>
-                        <p>Fecha de entrega: {" "}{pedido.destinatario.fechaEntrega}</p>
-                    </div>
-                    <div className="section">
-                        <h3>Detalles del pastel</h3>
-                        <p>Tamano: {" "} {pedido.pastel.tamano}</p>
-                        <p>Pan: {" "} {pedido.pastel.pan}</p>
-                        <p>Relleno: {" "} {pedido.pastel.relleno}</p>
-                        <p>Extras: {" "} {pedido.pastel.extras}</p>
-                        <p>Color del betun: {" "} {pedido.pastel.betun}</p>
-                        <p>Decoracion: {" "} {pedido.pastel.decoracion}</p>
-                    </div>
-                    <div className="section">
-                        <h3>Pago</h3>
-                        <p>Total: {" "}${pedido.pago.total}</p>
-                        <p>Metodo de pago: {" "} {pedido.pago.metodo}</p>
-                    </div>
+                )}
 
-                    <button
-                        onClick={() => navigate(`/edit-order/${pedido.pedidoId}`)}
-                        className="delete-button"
-                    >
-                        Editar pedido
-                    </button>
+                {pedido &&(
+                    <div className="result-card">
+                        <h2 className="order-title">Resultado de la busqueda</h2>
+                        <div className="section">
+                            <h3>Informacion del cliente</h3>
+                            <p>Nombre: {" "} {pedido.cliente.nombre}</p>
+                            <p>Email: {" "} {pedido.cliente.email}</p>
+                            <p>Telefono: {" "} {pedido.cliente.telefono}</p>
+                        </div>
+                        <div className="section">
+                            <h3>Informacion del destinatario</h3>
+                            <p>Nombre:{" "}{pedido.destinatario.nombre}</p>
+                            <p>Direccion:
+                                {" "}{pedido.destinatario.calle}
+                                {" "}{pedido.destinatario.numero}
+                                {" "}{pedido.destinatario.colonia}
+                            </p>
+                            <p>Fecha de entrega: {" "}{pedido.destinatario.fechaEntrega}</p>
+                        </div>
+                        <div className="section">
+                            <h3>Detalles del pastel</h3>
+                            <p>Tamano: {" "} {pedido.pastel.tamano}</p>
+                            <p>Pan: {" "} {pedido.pastel.pan}</p>
+                            <p>Relleno: {" "} {pedido.pastel.relleno}</p>
+                            <p>Extras: {" "} {pedido.pastel.extras}</p>
+                            <p>Color del betun: {" "} {pedido.pastel.betun}</p>
+                            <p>Decoracion: {" "} {pedido.pastel.decoracion}</p>
+                        </div>
+                        <div className="section">
+                            <h3>Pago</h3>
+                            <p>Total: {" "}$250</p>
+                            <p>Metodo de pago: {" "}{pedido.pago.metodo}</p>
+                        </div>
 
-                </div>
-            )}
+                        <button
+                            onClick={() => navigate(`/edit-order/${pedido.pedidoId}`)}
+                            className="step-button"
+                        >
+                            Editar pedido
+                        </button>
 
-            <button onClick={logout}>
+                    </div>
+                )}
+
+            <button className= "logout-button" onClick={logout}>
                 Cerrar sesion
             </button>
             
